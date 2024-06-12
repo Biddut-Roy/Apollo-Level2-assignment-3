e8ff578b7e5082e2e7633bb8e11e3437adeb7841
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-06-12 22:36:08 +0600

    file structure setup complete

---
+++ src/App.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
+
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('server Is a Starting');
+});
+
+// application routes
+app.use()
+
+//route not found
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
+
+export default app;
