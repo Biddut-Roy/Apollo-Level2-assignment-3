6b9a27e3dc525bd3d7bb0cf8785e1b3c7ed0a522
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-06-14 22:05:24 +0600

    auth checker setup facility route

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -5,6 +5,7 @@ import config from '../config';
 import AppError from '../errors/appError';
 import { TUserRole } from '../modules/Auth/auth.interface';
 import { catchAsync } from '../utils/catchAsync';
+import User from '../modules/Auth/auth.model';
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -17,27 +18,19 @@ const auth = (...requiredRoles: TUserRole[]) => {
 
     // checking if the given token is valid
     const decoded = jwt.verify(
-      token,
+      token.replace('Bearer ', ''),
       config.jwt_access_secret as string,
     ) as JwtPayload;
 
-    const { role, email, iat } = decoded;
+    const { role, email } = decoded;
 
-    // checking if the user is exist
-    const user = await User.isUserExistsByCustomId(email);
+    console.log(decoded);
+
+    // select('+password') use this method to  get password hidden model select :0
+    const user = await User.findOne({ email }).select('+password');
 
     if (!user) {
       throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
-    }
-
-    if (
-      user.passwordChangedAt &&
-      User.isJWTIssuedBeforePasswordChanged(
-        user.passwordChangedAt,
-        iat as number,
-      )
-    ) {
-      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
     }
 
     if (requiredRoles && !requiredRoles.includes(role)) {
@@ -49,7 +42,7 @@ const auth = (...requiredRoles: TUserRole[]) => {
 
     req.user = decoded as JwtPayload;
 
-    next();
+    // next();
   });
 };
 
--- src/app/modules/Facility/facility.route.ts
+++ src/app/modules/Facility/facility.route.ts
@@ -2,24 +2,29 @@ import express from 'express';
 import { FacilityControllers } from './facility.controller';
 import validationRequest from '../../middleware/validationRequest';
 import { facilityValidationZod } from './facility.validation';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../Auth/auth.constant';
 
 const router = express.Router();
 
 router.get('/', FacilityControllers.getAllFacility);
 router.post(
   '/',
+  auth(USER_ROLE.admin),
   validationRequest(facilityValidationZod.createFacilitySchemaZod),
   FacilityControllers.createFacility,
 );
 
 router.put(
   '/:id',
+  auth(USER_ROLE.admin),
   validationRequest(facilityValidationZod.updateFacilitySchemaZod),
   FacilityControllers.updateFacility,
 );
 
 router.delete(
   '/:id',
+  auth(USER_ROLE.admin),
   validationRequest(facilityValidationZod.deleteFacilitySchemaZod),
   FacilityControllers.deleteFacility,
 );
